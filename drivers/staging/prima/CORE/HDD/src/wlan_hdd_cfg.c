b0c1960ac8d80f1684d77521a53192f146c124c7
percy-g2 <gahlotpercy@gmail.com>
2015-05-05 11:56:09 +0200

    Revert "msm8916: merge lollipop-5.0.2-release-styxlte opensource-wlan-prima"
    This reverts commit e95215d7aea55a8ad3ee1ad9b6b1c75afdf917c7.

--- drivers/staging/prima/CORE/HDD/src/wlan_hdd_cfg.c
+++ drivers/staging/prima/CORE/HDD/src/wlan_hdd_cfg.c
@@ -3633,7 +3633,9 @@ static void print_hdd_cfg(hdd_context_t *pHddCtx)
   VOS_TRACE(VOS_MODULE_ID_HDD, VOS_TRACE_LEVEL_INFO_HIGH, "Name = [gMaxConcurrentActiveSessions] Value = [%u] ", pHddCtx->cfg_ini->gMaxConcurrentActiveSessions);
   VOS_TRACE(VOS_MODULE_ID_HDD, VOS_TRACE_LEVEL_INFO_HIGH, "Name = [gAcsScanBandPreference] Value = [%u] ",pHddCtx->cfg_ini->acsScanBandPreference);
   VOS_TRACE (VOS_MODULE_ID_HDD, VOS_TRACE_LEVEL_INFO_HIGH,"Name = [gACSBandSwitchThreshold] value = [%u]\n",pHddCtx->cfg_ini->acsBandSwitchThreshold);
+#ifdef FEATURE_WLAN_TDLS
   VOS_TRACE(VOS_MODULE_ID_HDD, VOS_TRACE_LEVEL_INFO_HIGH, "Name = [gEnableTDLSScan] value = [%u]\n",pHddCtx->cfg_ini->fEnableTDLSScan);
+#endif
 }
 
 
